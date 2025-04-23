4046c108b4be07fe30f37128606c4eb197e6a1c7
Shay Keren <shay.keren@gmail.com>
2025-04-23 16:24:00 +0300

    support api_key as part of the url

--- src/DigmaSSEServer/Program.cs
+++ src/DigmaSSEServer/Program.cs
@@ -1,23 +1,38 @@
-using System.Net;
 using DigmaSSEServer;
+using DigmaSSEServer.Authentication;
+using DigmaSSEServer.Options;
 using DigmaSSEServer.Tools;
+using Microsoft.Extensions.Options;
 using ModelContextProtocol.Protocol.Types;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDigma(builder.Configuration);
-
 builder.Services
-    .AddMcpServer(options => options.ServerInfo = new Implementation()
+    .AddMcpServer(options =>
     {
-        Name = "Digma Server",
-        Version = "1.0"
+        options.ServerInfo = new Implementation()
+        {
+            Name = "Digma Server",
+            Version = "1.0"
+            
+        };
     })
     .WithHttpTransport() // OR WithStdioServerTransport()
     .WithTools<CodeObservabilityTool>();
 
 var app = builder.Build();
+var authOptions = app.Services.GetRequiredService<IOptions<AuthOptions>>();
+if (string.IsNullOrWhiteSpace(authOptions.Value.Token))
+{
+    Console.WriteLine("ERROR: Missing required configuration 'Auth__Token must be specified'");
+    Environment.Exit(1); // Exit with failure code
+    return;
+}
 
-app.MapMcp();
+app.MapMcp(Constants.RoutePatternPrefix);
+
+
+app.UseMiddleware<ApiKeyValidatorMiddleware>();
 
 app.Run();
